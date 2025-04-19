4256f7f8ce43faea4e9ff9c0e79ab9fa92459a80
mgembree <mgembree@ucsc.edu>
2025-04-18 18:19:02 -0700

    Uploaded
    I uploaded the files !

---
+++ engine.js
@@ -0,0 +1,78 @@
+class Engine {
+
+    static load(...args) {
+        window.onload = () => new Engine(...args);
+    }
+
+    constructor(firstSceneClass, storyDataUrl) {
+        this.inventory = []; //items player has
+        this.flags = {};     
+
+        this.firstSceneClass = firstSceneClass;
+        this.storyDataUrl = storyDataUrl;
+
+        this.header = document.body.appendChild(document.createElement("h1"));
+        this.output = document.body.appendChild(document.createElement("div"));
+        this.actionsContainer = document.body.appendChild(document.createElement("div"));
+
+        fetch(storyDataUrl).then(
+            (response) => response.json()
+        ).then(
+            (json) => {
+                this.storyData = json;
+                this.gotoScene(firstSceneClass)
+            }
+        );
+    }
+
+    hasItem(itemName) {
+        return this.inventory.includes(itemName);
+    }
+    setFlag(flagName, value) {
+        this.flags[flagName] = value;
+    }
+    getFlag(flagName) {
+        return this.flags[flagName] || false;
+    }
+
+    gotoScene(sceneClass, data) {
+        this.scene = new sceneClass(this);
+        this.scene.create(data);
+    }
+
+    addChoice(action, data) {
+        let button = this.actionsContainer.appendChild(document.createElement("button"));
+        button.innerText = action;
+        button.onclick = () => {
+            while(this.actionsContainer.firstChild) {
+                this.actionsContainer.removeChild(this.actionsContainer.firstChild)
+            }
+            this.scene.handleChoice(data);
+        }
+    }
+
+    setTitle(title) {
+        document.title = title;
+        this.header.innerText = title;
+    }
+
+    show(msg) {
+        let div = document.createElement("div");
+        div.innerHTML = msg;
+        this.output.appendChild(div);
+    }
+}
+
+class Scene {
+    constructor(engine) {
+        this.engine = engine;
+    }
+
+    create() { }
+
+    update() { }
+
+    handleChoice(action) {
+        console.warn('no choice handler on scene ', this);
+    }
+}
---
+++ rules.js
@@ -0,0 +1,69 @@
+class Start extends Scene {
+    create() {
+        this.engine.setTitle(this.engine.storyData.Title);
+        this.engine.addChoice("Begin your investigation");
+    }
+
+    handleChoice() {
+        this.engine.gotoScene(Location, this.engine.storyData.InitialLocation);
+    }
+}
+
+class Location extends Scene {
+    create(key) {
+        let locationData = this.engine.storyData.Locations[key];
+        this.engine.show(locationData.Body);
+        
+        if (locationData.Choices && locationData.Choices.length > 0) {
+            for (let choice of locationData.Choices) {
+                if (!choice.Requires || this.checkCondition(choice.Requires)) {
+                    this.engine.addChoice(choice.Text, choice);
+                }
+            }
+        } else {
+            this.engine.addChoice("The end.");
+        }
+    }
+        checkCondition(requirements) {
+            //If player hasnt found the item 
+            if (requirements.item && !this.engine.hasItem(requirements.item)) {
+                return false;
+            }
+            //If player hasnt interacted with key object
+            if (requirements.flag && !this.engine.getFlag(requirements.flag)) {
+                return false;
+            }
+            return true;
+        }
+    
+    handleChoice(choice) {
+        // Handle any actions (item collection, flag setting) first
+        if (choice && choice.Action) {
+            if (choice.Action.addItem) {
+                this.engine.inventory.push(choice.Action.addItem);
+                this.engine.show(`> You acquired the ${choice.Action.addItem}!`);
+            }
+            if (choice.Action.setFlag) {
+                this.engine.setFlag(choice.Action.setFlag, true);
+                this.engine.show("> Something changed...");
+            }
+        }
+        
+        // Then proceed with navigation
+        if (choice && choice.Target) {
+            this.engine.show("> " + choice.Text);
+            this.engine.gotoScene(Location, choice.Target);
+        } else {
+            this.engine.gotoScene(End);
+        }
+    }
+}  
+
+class End extends Scene {
+    create() {
+        this.engine.show("<hr>");
+        this.engine.show(this.engine.storyData.Credits);
+    }
+}
+
+Engine.load(Start, 'myStory.json');
